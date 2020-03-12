f2c894e0dbc01507671a6124e07104dd569f8deb
urlordjames <urlordjames@gmail.com>
2020-03-12 11:55:09 -0400

    made level importing less bad

--- tools/importlvls.py
+++ tools/importlvls.py
@@ -1,11 +1,11 @@
+import json
+
 export = ["main.level"]
 
 for level in export:
     f = open("./levels/" + level, "r")
-    lvl = "const level = " + f.read()
+    lvl = dict(f.read())
     f.close()
     f = open("./src/" + level[:-5] + "js", "w")
-    lvl = lvl.replace("'dynamic': True", "'dynamic': true")
-    lvl = lvl.replace("'dynamic': False", "'dynamic': false")
-    f.write(lvl)
+    f.write("const level = " + json.dumps(lvl))
     f.close()
